041ab910abfca925b11ce5c8a49ef236018bf870
nekok500 <nekok500@proton.me>
2026-03-15 14:44:17 +0900

    a

--- public/read.php
+++ public/read.php
@@ -1,6 +1,6 @@
 <?php
 header('Access-Control-Allow-Credentials: true');
-header('Access-Control-Allow-Origin: http://test.localhost:8000');
+header('Access-Control-Allow-Origin: http://samesite-b.internal:8000');
 header('Access-Control-Allow-Methods: GET, POST, PUT, DELETE, OPTIONS');
 header('Access-Control-Allow-Headers: Content-Type, Authorization, Cookie');
 echo $_COOKIE['test'];
